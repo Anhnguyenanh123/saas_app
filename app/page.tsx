e1ebe90cbbacd8ddf78b355c896a66a903c00b5a
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2025-06-17 02:21:51 +0700

    implemented authentication, billing, database set up and create companion server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,48 @@
-import React from 'react'
-import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
     return (
-        <div>
-            <h1 className="text-2xl underline">Page</h1>
-            <Button>
-            </Button>
-        </div>
+        <main>
+            <h1 className="text-2xl underline">Popular Section</h1>
+            <section className="home-section">
+                <CompanionCard
+                    id="1"
+                    name="Sasha"
+                    topic="React"
+                    subject="JavaScript"
+                    duration="100"
+                    color="#ffda6e"
+                />
+                <CompanionCard
+                    id="2"
+                    name="Sasha"
+                    topic="React"
+                    subject="JavaScript"
+                    duration="100"
+                    color="#ffda6e"
+                />
+                <CompanionCard
+                    id="2"
+                    name="Sasha"
+                    topic="React"
+                    subject="JavaScript"
+                    duration="100"
+                    color="#ffda6e"
+                />
+            </section>
+
+            <section className="home-section">
+                <CompanionList
+                    title="Recent Session"
+                    companions={recentSessions}
+                    className="w-2/3 max-lg:w-full"
+                />
+                <CTA/>
+            </section>
+        </main>
     )
 }
 export default Page
